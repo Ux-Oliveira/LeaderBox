9d6bd6d2c5e1c20348fed170fe79326e6696af3e
Henrique O. <lemonheadch@outlook.com>
2025-11-27 10:10:09 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css';
+import App from './App.jsx';
+import { BrowserRouter } from "react-router-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
