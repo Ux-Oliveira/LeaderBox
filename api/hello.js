fd8485a7e42a23b9801a3f7fa6d2ea52abaa2d69
Ux-Oliveira <lemonheadch@outlook.com>
2025-11-29 18:01:25 -0300

    chore: add hello test endpoint and tiktok exchange handler

---
+++ api/hello.js
@@ -0,0 +1,5 @@
+﻿export default function handler(req, res) {
+  console.log("hello function invoked", { method: req.method });
+  res.setHeader("Content-Type", "application/json");
+  return res.status(200).json({ ok: true, msg: "hello from serverless" });
+}
