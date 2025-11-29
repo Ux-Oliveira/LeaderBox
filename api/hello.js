da617ee637a23c925da84213d02d7886dff73443
Henrique O. <lemonheadch@outlook.com>
2025-11-29 17:55:48 -0300

    Create hello.js

---
+++ api/hello.js
@@ -0,0 +1,6 @@
+// /api/hello.js
+export default function handler(req, res) {
+  console.log("hello function invoked", { method: req.method });
+  res.setHeader("Content-Type", "application/json");
+  return res.status(200).json({ ok: true, msg: "hello from serverless" });
+}
