43d2b5e434bcc0090311bc5dd4cd5d5ae048c392
Henrique O. <lemonheadch@outlook.com>
2025-11-30 19:34:10 -0300

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,16 +1,10 @@
-// server/config.js
-import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
-const router = express.Router();
-
-router.get("/config.js", (req, res) => {
-  const publicEnv = {
+// server/index.js
+app.get("/config.js", (req, res) => {
+  const envVars = {
     VITE_TIKTOK_CLIENT_KEY: process.env.VITE_TIKTOK_CLIENT_KEY || "",
     VITE_TIKTOK_REDIRECT_URI: process.env.VITE_TIKTOK_REDIRECT_URI || ""
   };
+  const js = `window.__ENV = ${JSON.stringify(envVars)};`;
   res.setHeader("Content-Type", "application/javascript");
-  res.send(`window.__ENV = ${JSON.stringify(publicEnv)};`);
+  res.send(js);
 });
-
-export default router;
