f324afabe308f13c2a1303c2e17184607c4014b2
Henrique O. <lemonheadch@outlook.com>
2025-11-30 12:36:52 -0300

    Delete api directory

--- api/hello.js
+++
@@ -1,5 +0,0 @@
-﻿export default function handler(req, res) {
-  console.log("hello function invoked", { method: req.method });
-  res.setHeader("Content-Type", "application/json");
-  return res.status(200).json({ ok: true, msg: "hello from serverless" });
-}
