df4bfe612cc290540f06cff3c708b88ef002287d
Henrique O. <lemonheadch@outlook.com>
2025-11-30 21:49:54 -0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,10 +1,4 @@
-// server/index.js
-app.get("/config.js", (req, res) => {
-  const envVars = {
-    VITE_TIKTOK_CLIENT_KEY: process.env.VITE_TIKTOK_CLIENT_KEY || "",
-    VITE_TIKTOK_REDIRECT_URI: process.env.VITE_TIKTOK_REDIRECT_URI || ""
-  };
-  const js = `window.__ENV = ${JSON.stringify(envVars)};`;
-  res.setHeader("Content-Type", "application/javascript");
-  res.send(js);
-});
+window.__ENV = {
+  VITE_TIKTOK_CLIENT_KEY: "awjs5urmu24dmwqc",
+  VITE_TIKTOK_REDIRECT_URI: "https://leaderbox.co/auth/tiktok/callback"
+};
