ac492c787607429e4423046e5dbb5006dca3177d
Henrique O. <lemonheadch@outlook.com>
2025-12-03 13:09:09 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // proxy all /api requests to your Express server during dev
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:4000',
-        changeOrigin: true,
-        secure: false,
-        // keep paths as-is
-      },
-    },
-  },
 })
