fc6d136217c79180adbb7eeb105ef12c72db9e75
Henrique O. <lemonheadch@outlook.com>
2025-12-07 16:52:06 -0300

    Update Privacy.jsx

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -26,9 +26,14 @@ export default function Privacy() {
           <p><strong>5. Your Rights</strong><br />
           You can request deletion of your data at any time.</p>
 
-          <p><strong>6. Contact</strong><br />
+          <p><strong>6. Contact Information</strong><br />
             <a href="https://www.tiktok.com/@ricks_a_human" target="_blank" style={{ color: "var(--accent)" }}>@Ricks_a_human</a> on TikTok for management suppport.<br />
           Email: <a href="mailto:leaderbox_management@outlook.com" style={{ color: "var(--accent)" }}>leaderbox_management@outlook.com</a> for legal or business porposes.</p>
+
+          <p><strong>7. Support the website!
+          Leaderbox is not a paid service. Any form of support is voluntary and could be done so via subscribing to Rick's a Human's <a href="https://www.youtube.com/@ricksahuman" target="_blank" style={{ color: "var(--accent)" }}>Patreon</a>
+            or <a href="https://www.youtube.com/@ricksahuman" target="_blank" style={{ color: "var(--accent)" }}>Youtube!</a><br />
+            Please also support <a className="small" href="https://www.youtube.com/@JangoDisc" target="_blank" rel="noreferrer" style={{color:"var(--accent)",marginLeft:8}}>JangoDisc</a>on Youtube, as his visual style was the inspiration for the website.</strong></p>
         </div>
       </div>
     </div>
