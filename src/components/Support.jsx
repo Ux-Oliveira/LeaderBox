061452ba5ff9a3673a3632b4e7d5a79ef5d54a13
Henrique O. <lemonheadch@outlook.com>
2025-12-07 12:12:10 -0300

    Update Support.jsx

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -5,24 +5,22 @@ export default function Support(){
      <div>
         <div className="support card">
             <div className="left">
-                <div className="small">Based on JangoDisc's video!
-                <a className="small" href="https://www.youtube.com" target="_blank" rel="noreferrer" style={{color:"var(--accent)",marginLeft:8}}>
+                <div className="small">Based on JangoDisc's video!</div>
+                <a className="small" href="https://www.youtube.com" target="_blank" rel="noreferrer" style={{color:"var(--accent)",marginLeft:8, display: "flex", alignItems: "center", gap: 6}}>
                     <i className="fa fa-youtube"/> Youtube {/*I'll setup the fontawesome icon later*/}
                 </a>
             </div>
 
             <div style={{flex:1}} />
-            
-            <div className="righ">
-                <a className="small" href="https://www.youtube.com" target="_blank" rel="noreferrer" style={{color:"var(--accent)",marginRight:8}}>
+
+            <div className="right" style={{display: "flex", alignItems: "center", gap: 8}}>
+                <div className="small">Website by Rick's a Human</div>
+                <a className="small" href="https://www.youtube.com" target="_blank" rel="noreferrer" style={{color:"var(--accent)",marginLeft:8, display: "flex", alignItems: "center", gap: 6}}>
                     <i className="fa fa-youtube"/> Youtube
                 </a>
-                <div className="small">Website by Rick's a Human</div>
             </div>
          </div>
         </div>
      </div>
     );
 }
-
-{/*BOTH THE SUPPORT AND THE VIDEO SECTION FILES ARE PRETTY BASIC AT THE MOMENT CAUSE ILL FIRST FOCUS ON GETTING THE PROFILE SET UP AND MANAGEMENT SIDE OF THINGS READY*/}
