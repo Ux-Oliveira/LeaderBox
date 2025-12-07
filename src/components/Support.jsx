f76ac43ce89e6952f20d41b2cf6ca52875c4fdda
Henrique O. <lemonheadch@outlook.com>
2025-12-07 16:11:47 -0300

    Update Support.jsx

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 
-export default function Support(){
-    return (
-     <div>
-        <div className="support card">
-            <div className="left">
-                <div className="small">Inspired by <a className="small" href="https://www.youtube.com" target="_blank" rel="noreferrer" style={{color:"var(--accent)",marginLeft:8}}>JangoDisc</a> | Website by 
-                    <a className="small" href="https://www.youtube.com" target="_blank" rel="noreferrer" style={{color:"var(--accent)",marginLeft:8}}>Rick's a Human</a>
-            </div>
-                <div style={{flex:1}} />
-         </div>
+export default function Support() {
+  return (
+    <div className="support-center-wrapper">
+      <div className="support card">
+        <div className="left">
+          <div className="small">
+            Inspired by{" "}
+            <a
+              className="small"
+              href="https://www.youtube.com/@JangoDisc"
+              target="_blank"
+              rel="noreferrer"
+              style={{ color: "var(--accent)", marginLeft: 8 }}
+            >
+              JangoDisc
+            </a>{" "}
+            | Website by
+            <a
+              className="small"
+              href="https://www.youtube.com/@ricksahuman"
+              target="_blank"
+              rel="noreferrer"
+              style={{ color: "var(--accent)", marginLeft: 8 }}
+            >
+              Rick's a Human
+            </a>
+          </div>
         </div>
-     </div>
-    );
+      </div>
+    </div>
+  );
 }
-
-{/*BOTH THE SUPPORT AND THE VIDEO SECTION FILES ARE PRETTY BASIC AT THE MOMENT CAUSE ILL FIRST FOCUS ON GETTING THE PROFILE SET UP AND MANAGEMENT SIDE OF THINGS READY*/}
-
