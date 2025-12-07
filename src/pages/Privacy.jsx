e3dab0a1fcaa54387ca6b45473a5399e0e52bab6
Henrique O. <lemonheadch@outlook.com>
2025-12-07 15:36:57 -0300

    Update Privacy.jsx

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -8,23 +8,26 @@ export default function Privacy() {
 
         <div style={{ marginTop: 12, color: "rgba(255,255,255,0.92)", lineHeight: 1.5 }}>
           <p><strong>1. Information We Collect</strong><br />
-          - TikTok login info (username, user ID, profile picture)<br />
-          - Any data you provide within the app</p>
+          TikTok login info (username, user ID, profile picture)<br />
+          - Leaderbox collects you Tiktok data for login and content sharing porpuses only. Upon signing up the user must create an ID through which they'll be identified.
+            Leaderbox won't display your Tiktok page withing the website.</p>
 
           <p><strong>2. How We Use Information</strong><br />
-          - To personalize your experience<br />
-          - To improve our app</p>
+          To personalize your experience<br />
+          To improve our website by making it easy to sign in.
+          And to facilitate content sharing.</p>
 
           <p><strong>3. Data Sharing</strong><br />
-          - We do not sell or share your personal data with third parties</p>
+          We do not sell or share your personal data with third parties in any way!</p>
 
           <p><strong>4. Data Security</strong><br />
-          - We implement measures to protect your data</p>
+          We implement measures to protect your data</p>
 
           <p><strong>5. Your Rights</strong><br />
-          - You can request deletion of your data at any time</p>
+          You can request deletion of your data at any time.</p>
 
           <p><strong>6. Contact</strong><br />
+            <a href="https://www.tiktok.com/@ricks_a_human" target="_blank" style={{ color: "var(--accent)" }}>@Ricks_a_human</a> on TikTok for management suppport.
           Email: <a href="mailto:leaderbox_management@outlook.com" style={{ color: "var(--accent)" }}>leaderbox_management@outlook.com</a></p>
         </div>
       </div>
