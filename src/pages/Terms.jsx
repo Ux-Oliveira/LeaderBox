ffa09504f175af48e16104d391d29d09c1bddb93
Henrique O. <lemonheadch@outlook.com>
2025-12-07 16:52:28 -0300

    Update Terms.jsx

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -28,10 +28,15 @@ export default function Terms() {
           <p><strong>5. Termination</strong><br />
           We may suspend accounts for violations.</p>
 
-          <p><strong>6. Contact</strong><br />
+          <p><strong>6. Contact Information</strong><br />
           You can contact me via email for legal or business porposes.<br />Or via TikTok (<a href="https://www.tiktok.com/@ricks_a_human"
                                                                                         target="_blank" style={{ color: "var(--accent)" }}>@Ricks_a_human</a>) for management suppport.<br />  
           Email: <a href="mailto:leaderbox_management@outlook.com" style={{ color: "var(--accent)" }}>leaderbox_management@outlook.com</a></p>
+
+          <p><strong>7. Support the website!
+          Leaderbox is not a paid service. Any form of support is voluntary and could be done so via subscribing to Rick's a Human's <a href="https://www.youtube.com/@ricksahuman" target="_blank" style={{ color: "var(--accent)" }}>Patreon</a>
+            or <a href="https://www.youtube.com/@ricksahuman" target="_blank" style={{ color: "var(--accent)" }}>Youtube!</a><br />
+            Please also support <a className="small" href="https://www.youtube.com/@JangoDisc" target="_blank" rel="noreferrer" style={{color:"var(--accent)",marginLeft:8}}>JangoDisc</a>on Youtube, as his visual style was the inspiration for the website.</strong></p>
         </div>
       </div>
     </div>
