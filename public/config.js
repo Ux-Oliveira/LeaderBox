dcade10766979d90fb6d2e12c9db0e3261e05cd9
Henrique O. <lemonheadch@outlook.com>
2025-12-08 11:23:38 -0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
-// public/config.js
 window.__ENV = {
   VITE_TIKTOK_CLIENT_KEY: "awjs5urmu24dmwqc",
   VITE_TIKTOK_REDIRECT_URI: "https://leaderbox.co/auth/tiktok/callback",
-  VITE_LETTERBOXD_CLIENT_KEY: "oqKLT2bpwn0gpTTTAHs0pBGbu22J2mB8",     
+  VITE_LETTERBOXD_CLIENT_ID: "oqKLT2bpwn0gpTTTAHs0pBGbu22J2mB8",
+  VITE_LETTERBOXD_AUTH0_DOMAIN: "dev-3ruj22gupabasu25.us.auth0.com",
   VITE_LETTERBOXD_REDIRECT_URI: "https://leaderbox.co/auth/letterboxd/callback",
   LEADERBOX_SERVER_BASE: "https://leaderbox.co"
 };
