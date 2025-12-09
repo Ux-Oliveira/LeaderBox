408a060043caf52552fcb1dabe66eeebe180afe0
Henrique O. <lemonheadch@outlook.com>
2025-12-09 16:29:40 -0300

    Update Duel.jsx

--- src/pages/Duel.jsx
+++ src/pages/Duel.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { fetchAllProfiles } from "../lib/api";
 import { useNavigate } from "react-router-dom";
-import Support from "./components/Support";
+import Support from "../components/Support";
 
 /*
   Duel.jsx - Full fixed file
@@ -359,7 +359,7 @@ export default function Duel() {
           </div>
         </div>
       )}
-      <Route path="./components/support" element={<Support />} />
+       <Support />
     </div>
   );
 }
