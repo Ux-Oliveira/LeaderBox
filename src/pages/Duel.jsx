fff7ad07ce31cf0f587ad09f39455e065528efe7
Henrique O. <lemonheadch@outlook.com>
2025-12-09 16:23:52 -0300

    Update Duel.jsx

--- src/pages/Duel.jsx
+++ src/pages/Duel.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { fetchAllProfiles } from "../lib/api";
 import { useNavigate } from "react-router-dom";
-import Support from "./Support";
+import Support from "./components/Support";
 
 /*
   Duel.jsx - Full fixed file
