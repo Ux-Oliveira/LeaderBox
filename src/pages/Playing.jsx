19322775128958a9d38abc284b8c42f5561f58bf
Henrique O. <lemonheadch@outlook.com>
2025-12-12 12:26:59 -0300

    Create Playing.jsx

---
+++ src/pages/Playing.jsx
@@ -0,0 +1,8 @@
+// src/pages/Playing.jsx
+import React from "react";
+
+export default function Playing() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", backgroundColor: "#101221" }} />
+  );
+}
